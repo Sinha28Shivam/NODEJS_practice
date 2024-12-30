ea94a272d603b2e8c5b76e9e3dbf21f618798957
sinha28shivam <sinha28.shivam@gmail.com>
2024-12-30 16:45:48 +0530

    installed mongoose and nodemon and configure

--- Project_01/NODEJS_practice/NodeJS/first.js
+++
@@ -1,7 +0,0 @@
-const {add, sub} = require("./math")
-
-
-//console.log("Math value is", math(2, 4));
-
-console.log("Math value is", add(2, 4));
-console.log("Math value is", sub(10, 2));
--- Project_01/index.js
+++ Project_01/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
 const fs = require("fs");
+const mongoose = require("mongoose");   
 
 const app = express();
 const PORT = 3000;
@@ -25,24 +26,37 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.route("/api/users/:id")
 .get((req, res) => {
+    res.setHeader("X-MyName", "Shivam Sinha");  // Custom Header Always Append x in custom header
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if(!user) return res.status(404).json({ Msg: "User Not Found" });
     return res.json(user);
 })
 .put((req, res) => {
     return res.json(user);
 })
 .delete((req, res) => {
+    
     return res.json(user)
 });
 
 
 app.post("/api/users", (req, res) => {
     const body = req.body;
-    users.push({ ...body, id: users.length +1 }); // increment id by default reading from the json file
 
+    if(!body || 
+        !body.first_name || 
+        !body.last_name || 
+        !body.email || 
+        !body.gender || 
+        !body.job_title){
+        return res.status(400).json({ Msg: "All field are required" });
+    }
+
+    users.push({ ...body, id: users.length +1 }); // increment id by default reading from the json file
+    
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-        return res.json({ Status: "Success", id: users.length });
+        return res.status(201).json({ Status: "Success", id: users.length });
     });
 
 
